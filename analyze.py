8504f68a8dd81fece2eaf37219a36dc534b5d571
Anas Moazzam <am10809@nyu.edu>
2024-02-13 00:56:54 -0500

    finished

--- analyze.py
+++ analyze.py
@@ -1,1 +1,22 @@
-import sys
+import csv
+
+f = open('data/clean_data.csv', 'r')
+csv_reader = csv.DictReader(f)
+years = list()
+totals = list()
+averages = list()
+
+for line in csv_reader:
+    years.append(line["Year"])
+    totals.append(float(line["J-D"]))
+total = 0
+decades = list()
+f.close()
+
+for i in range(9, len(totals), 10):
+    averages.append(sum(map(float, totals[i-9:i+1])) / 10)
+    decades.append(f"{years[i-9]}-{years[i]}")
+
+print("Decade:       Average Temperature Anamoly:")
+for i in range(len(decades)):
+    print(str(decades[i])+"                "  + str(format(averages[i],'.1f')) + " °F")
